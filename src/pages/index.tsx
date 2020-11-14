b9dc243452c2b25dd1e9ca27f05144e88eb1a435
JosueFS <josue.18rj@gmail.com>
2020-11-14 00:21:07 -0300

    API Fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const[recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+  
+
+  //FETCH: Quando não há necessidade de indexar informações para os motores de busca
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    });
+  },[]);
+
   return (
     <div>
-      <Title>Home</Title>
+        <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recProd => {
+            return (
+              <li key={recProd.id}>
+                {recProd.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
